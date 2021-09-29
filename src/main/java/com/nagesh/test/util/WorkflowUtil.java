927d240387e2163c69ba27a10e15839d84e3f68f
Nagesh Ramaswamy <nagesh621@gmail.com>
2021-09-29 19:24:33 +0530

    Get task issue fix

--- src/main/java/com/nagesh/test/util/WorkflowUtil.java
+++ src/main/java/com/nagesh/test/util/WorkflowUtil.java
@@ -4,14 +4,15 @@ import com.nagesh.test.entity.Process;
 import com.nagesh.test.entity.ProcessTask;
 import com.nagesh.test.repo.ProcessRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpEntity;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.MediaType;
-import org.springframework.http.ResponseEntity;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.http.*;
 import org.springframework.stereotype.Component;
 import org.springframework.util.LinkedMultiValueMap;
 import org.springframework.util.MultiValueMap;
 import org.springframework.web.client.RestTemplate;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Component
 public class WorkflowUtil {
@@ -44,9 +45,13 @@ public class WorkflowUtil {
         RestTemplate restTemplate = new RestTemplate();
         HttpHeaders headers = new HttpHeaders();
         headers.setContentType(MediaType.APPLICATION_JSON);
-        ProcessTask task = restTemplate.getForObject(uri, ProcessTask.class);
+        ResponseEntity<List<ProcessTask>> rateResponse =
+                restTemplate.exchange(uri,
+                        HttpMethod.GET, null, new ParameterizedTypeReference<List<ProcessTask>>() {
+                        });
+        List<ProcessTask> taskList = rateResponse.getBody();
 
-        uri="http://localhost:8080/engine-rest/task/"+task.getId()+"/complete";
+        uri="http://localhost:8080/engine-rest/task/"+ taskList.get(0).getId()+"/complete";
 
         MultiValueMap<String, String> map = new LinkedMultiValueMap<>();
         HttpEntity<MultiValueMap<String, String>> request = new HttpEntity<>(map, headers);
